cccfa5be9a3c638f5d5d73f7a04fa42ad7f02cda
Ankit Bhowmick <ankitbhowmick.9@gmail.com>
2022-09-17 21:00:31 +0530

    'ImplementTokenAPI'

--- app/core/admin.py
+++ app/core/admin.py
@@ -11,7 +11,6 @@ from core import models
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
 
-
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
@@ -28,5 +27,21 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
 
 admin.site.register(models.User, UserAdmin)
